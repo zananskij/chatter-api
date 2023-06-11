6059c64aea9585dfb41011845460605c056eb17b
zananskij <zananskij@gmail.com>
2023-06-11 16:29:05 -0400

    added readme.md

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// imports
 const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
@@ -12,20 +13,21 @@ const fs = require("fs")
 const { Router } = require("express")
 const { db } = require("./models/User")
 
-// env variable / db connection
+// load env variable + db connection
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL, (err) => {
   console.log("Connected to MongoDB")
   if (err) throw err
 })
+
 const jwtSecret = process.env.JWT_SECRET
 const bcryptSalt = bcrypt.genSaltSync(10)
 
 // express app setup
 const app = express()
 app.use("/uploads", express.static(__dirname + "/uploads"))
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json()) // parse request bodies as json
+app.use(cookieParser()) // parse cookie header and populate req.cookies
 app.use(
   cors({
     credentials: true,
@@ -36,14 +38,15 @@ app.use(
 // extract token from req cookie, verifies, then return userData. Otherwise reject promise
 async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) => {
-    const token = req.cookies?.token
+    const token = req.cookies?.token // extract token from request cookie
     if (token) {
       jwt.verify(token, jwtSecret, {}, (err, userData) => {
+        // verify token using JWT and secret key
         if (err) throw err
-        resolve(userData)
+        resolve(userData) // resolve with user data
       })
     } else {
-      reject("no token")
+      reject("no token") // reject with "no token" message
     }
   })
 }
@@ -58,46 +61,48 @@ app.get("/", (req, res) => {
 
 // get all msgs between current user and selected conversation
 app.get("/messages/:userId", async (req, res) => {
-  const { userId } = req.params
-  const userData = await getUserDataFromRequest(req)
-  const ourUserId = userData.userId
+  const { userId } = req.params // extract userId from request parameters
+  const userData = await getUserDataFromRequest(req) // get user data from request
+  const ourUserId = userData.userId // extract userId
   const messages = await Message.find({
     sender: { $in: [userId, ourUserId] },
     recipient: { $in: [userId, ourUserId] },
-  }).sort({ createdAt: 1 })
-  res.json(messages)
+  }).sort({ createdAt: 1 }) // find messages between the two users and sort by createdAt
+  res.json(messages) // respond with the messages
 })
 
 // get all users from DB, return _id & username
 app.get("/people", async (req, res) => {
-  const users = await User.find({}, { _id: 1, username: 1 })
-  res.json(users)
+  const users = await User.find({}, { _id: 1, username: 1 }) // finds all users and return only _id and username
+  res.json(users) // respond with the users
 })
 
 // get user's data if cookie contains valid token
 app.get("/profile", (req, res) => {
-  const token = req.cookies?.token
+  const token = req.cookies?.token // extract token from request cookie
   if (token) {
     jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      // verify token using JWT and secret key
       if (err) throw err
-      res.json(userData)
+      res.json(userData) // respond with user data
     })
   } else {
-    res.status(401).json("no token")
+    res.status(401).json("no token") // respond with "no token" status code
   }
 })
 
 // verifies valid credentials, new token is signed & sent back in response
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body
-  const foundUser = await User.findOne({ username })
+  const { username, password } = req.body // extract username and password from request body
+  const foundUser = await User.findOne({ username }) // find user by username
   if (foundUser) {
-    const passOk = bcrypt.compareSync(password, foundUser.password)
+    const passOk = bcrypt.compareSync(password, foundUser.password) // compare passwords using bcrypt
     if (passOk) {
       jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+        // sign new token using JWT and secret key
         res.cookie("token", token, { sameSite: "none", secure: true }).json({
           id: foundUser._id,
-        })
+        }) // sets token in response cookie and respond with user ID
       })
     }
   }
@@ -105,23 +110,23 @@ app.post("/login", async (req, res) => {
 
 // clear token with an empty string
 app.post("/logout", (req, res) => {
-  res.cookie("token", "", { sameSite: "none", secure: true }).json("ok")
+  res.cookie("token", "", { sameSite: "none", secure: true }).json("ok") //clear token
 })
 
 // hashes password and stores new user in db, new token is signed & sent
 app.post("/register", async (req, res) => {
-  const { username, password } = req.body
+  const { username, password } = req.body // extract username and password from request body
   try {
-    const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt) // hash password using bcrypt
     const createdUser = await User.create({
       username: username,
       password: hashedPassword,
-    })
+    }) // create the new user in the db
     jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
       if (err) throw err
       res.cookie("token", token, { sameSite: "none", secure: true }).status(201).json({
         id: createdUser._id,
-      })
+      }) // sets token in response cookie,
     })
   } catch (err) {
     if (err) throw err
@@ -138,6 +143,7 @@ const server = app.listen(port, () => {
 
 // new WS server created on same port
 const wss = new ws.WebSocketServer({ server })
+
 // listens for new WS connection
 wss.on("connection", (connection, req) => {
   function notifyAboutOnlinePeople() {
@@ -168,13 +174,14 @@ wss.on("connection", (connection, req) => {
   })
 
   // authenticate connection using token
-  const cookies = req.headers.cookie
+  const cookies = req.headers.cookie // extract cookies from request headers
   if (cookies) {
-    const tokenCookieString = cookies.split(";").find((str) => str.startsWith("token="))
+    const tokenCookieString = cookies.split(";").find((str) => str.startsWith("token=")) // find token cookie string
     if (tokenCookieString) {
-      const token = tokenCookieString.split("=")[1]
+      const token = tokenCookieString.split("=")[1] // extract token valuee
       if (token) {
         jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          // vErify token using JWT and secret key
           if (err) throw err
           const { userId, username } = userData
           connection.userId = userId
@@ -186,22 +193,23 @@ wss.on("connection", (connection, req) => {
 
   // listens for msgs from connection
   connection.on("message", async (message) => {
-    const messageData = JSON.parse(message.toString())
-    const { recipient, text, file } = messageData
+    const messageData = JSON.parse(message.toString()) // parses incoming message data
+    const { recipient, text, file } = messageData // Extract message details
     let filename = null
     if (file) {
-      console.log("size", file.data.length)
-      const parts = file.name.split(".")
+      console.log("size", file.data.length) // print file size
+      const parts = file.name.split(".") // split file name to extract extension
       const ext = parts[parts.length - 1]
       filename = Date.now() + "." + ext
       const path = __dirname + "/uploads/" + filename
-      const bufferData = new Buffer(file.data.split(",")[1], "base64")
+      const bufferData = new Buffer(file.data.split(",")[1], "base64") // converts file data from base64 to buffer
       fs.writeFile(path, bufferData, () => {
         console.log("file saved:" + path)
       })
     }
     if (recipient && (text || file)) {
       const messageDoc = await Message.create({
+        // adds new message document in the database
         sender: connection.userId,
         recipient,
         text,
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Chat Messenger - Backend
+
+## Overview
+
+This repository contains the backend code for my chatter messenger app. It provides the server-side functionality required for user authentication, message handling, and WebSocket communication. The chat messenger app is built using Node.js and Express.js for the server-side implementation. It utilizes MongoDB as the database for storing user information and messages. WebSocket is used to enable real-time communication between users.
+
+## Deployed Link (Backend / API)
+
+https://chatter-messenger-backend.herokuapp.com/
+
+## Skills Utilized
+
+- Node.js
+- Express.js
+- MongoDB
+- WebSocket
+- JSON Web Tokens (JWT)
+- Bcrypt.js
+- File system operations / sharing
+
+## Contact me!
+
+Please feel free to explore the code and reach out if you have any questions or feedback!
+
+- https://jonzananski.herokuapp.com/
+- https://www.linkedin.com/in/zananskij/
